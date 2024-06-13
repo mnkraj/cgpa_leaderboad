f8e11362e4f2d50b60458907a8b547586f6c3798
Mayank Raj <mayankraj7100@gmail.com>
2024-06-14 04:21:37 +0530

    Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./Designer.png";
 
 const Navbar = ({ search, setsearch }) => {
+  const [toggle,settoggle] = useState(false)
   return (
     <div>
       <nav className="border-gray-200 bg-gray-900">
@@ -70,7 +71,10 @@ const Navbar = ({ search, setsearch }) => {
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
-              aria-expanded="false"
+              aria-expanded="true"
+              onChange={(e)=>{
+                settoggle(!toggle)
+              }}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -91,10 +95,10 @@ const Navbar = ({ search, setsearch }) => {
             </button>
           </div>
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-            <div className="relative mt-3 md:hidden">
+            <div className={`relative mt-3 md:hidden ${toggle ? "md:hidden" : ""}`}>
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  className="w-4 h-4 text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -112,13 +116,14 @@ const Navbar = ({ search, setsearch }) => {
               <input
                 type="text"
                 id="search-navbar"
-                value={search}
-                
+                onChange={(e) => {
+                  setsearch(e.target.value);
+                }}
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
               />
             </div>
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 ">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-900 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 ">
               <li>
                 <a
                   href="#"
