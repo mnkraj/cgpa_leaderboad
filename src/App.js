8ac883eaf744409151f9d437798c1abf490f654c
Mayank Raj <mayankraj7100@gmail.com>
2024-06-14 05:04:15 +0530

    .

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ const App = () => {
                         className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
                         style={{ width: "10%" }}
                       >
-                        #
+                        SI No.
                       </th>
                       <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                         Regn No
