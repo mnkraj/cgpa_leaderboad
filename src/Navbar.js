49b1e9044a5661c5b9333a2f7f2d000884622727
Mayank Raj <mayankraj7100@gmail.com>
2024-08-04 19:10:03 +0530

    Result Portal

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
 import logo from "./Designer.png";
-import { TypeAnimation } from 'react-type-animation';
-const Navbar = ({ search, setsearch , disab }) => {
-  
+import { TypeAnimation } from "react-type-animation";
+
+const Navbar = ({ search, setsearch, disab }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleResultPortalClick = (useLan) => {
+    const url = useLan
+      ? "http://172.16.1.17/StudentPortal/Login.aspx"
+      : "http://202.168.87.90/StudentPortal/Login.aspx";
+    window.open(url, "_blank");
+  };
+
   return (
     <div>
       <nav className="border-gray-200 bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center">
             <span className="self-center text-2xl font-bold whitespace-nowrap text-white">
-            <TypeAnimation
-                sequence={[
-                  "CGPA LEADERBOARD",
-                  1000,
-                  "NIT JSR",
-                  1000,
-                ]}
+              <TypeAnimation
+                sequence={["CGPA LEADERBOARD", 1000, "NIT JSR", 1000]}
                 wrapper="span"
                 speed={50}
-                style={{ display: 'inline-block', whiteSpace: 'nowrap',width :"210px" }}
+                style={{
+                  display: "inline-block",
+                  whiteSpace: "nowrap",
+                  width: "210px",
+                }}
                 repeat={Infinity}
               />
             </span>
           </a>
           <div className="flex md:order-2">
-           
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
@@ -44,21 +51,21 @@ const Navbar = ({ search, setsearch , disab }) => {
                 </svg>
                 <span className="sr-only">Search icon</span>
               </div>
-              {<input
+              <input
                 type="text"
                 id="search-navbar1"
-                onChange={(e) => {
-                  setsearch(e.target.value);
-                }}
-                disabled = {!disab}
+                onChange={(e) => setsearch(e.target.value)}
+                disabled={!disab}
                 className="block w-full p-2 pl-10 text-sm text-white border border-gray-600 rounded-lg bg-gray-800 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Regn. no"
-              />}
+              />
             </div>
-            
           </div>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-            <div className={`relative mt-3 md:hidden`}>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-search"
+          >
+            <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-400"
@@ -76,17 +83,14 @@ const Navbar = ({ search, setsearch , disab }) => {
                   />
                 </svg>
               </div>
-              {<input
+              <input
                 type="text"
                 id="search-navbar"
-                onChange={(e) => {
-                  setsearch(e.target.value);
-                  
-                }}
-                disabled = {!disab}
+                onChange={(e) => setsearch(e.target.value)}
+                disabled={!disab}
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
-              />}
+              />
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-900 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 ">
               <li>
@@ -97,6 +101,38 @@ const Navbar = ({ search, setsearch , disab }) => {
                 >
                   Home
                 </a>
+              </li>
+              <li className="relative block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-gray-900 dark:border-gray-700">
+                <button
+                  className="inline-flex items-center"
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                >
+                  Result Portal
+                </button>
+                {dropdownOpen && (
+                  <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleResultPortalClick(true);
+                      }}
+                    >
+                      Institute LAN/WIFI
+                    </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleResultPortalClick(false);
+                      }}
+                    >
+                      Mobile Networks
+                    </a>
+                  </div>
+                )}
               </li>
               <li>
                 <a
