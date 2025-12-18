c144d2980ea3c56e46725e367ef87288521a3455
Mayank Raj <mayankraj7100@gmail.com>
2025-12-18 12:01:41 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -99,7 +99,7 @@ const Home = () => {
                 onChange={(e) => setYear(e.target.value)}
               >
                 <option value="">All Year</option>
-                <option value="2024">2025</option>
+                <option value="2025">2025</option>
                 <option value="2024">2024</option>
                 <option value="2023">2023</option>
                 <option value="2022">2022</option>
